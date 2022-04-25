50a278659e098843c3c33926b4f1614218055e70
sintaeyoung <86948066+sintaeyoung@users.noreply.github.com>
2022-04-25 13:57:23 +0900

    taeyoung backend

---
+++ src/main/java/com/editorimage/editorbackend/EditorbackendApplication.java
@@ -0,0 +1,13 @@
+package com.editorimage.editorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EditorbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/editorimage/editorbackend/EditorbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.editorimage.editorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditorbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
